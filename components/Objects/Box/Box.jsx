38d11126f8c2b1bc92aa3642e10d8650167bcea9
Sharone Nixon <shanix@live.de>
2022-07-12 10:29:56 +0200

    added drag controls & texture

--- components/Objects/Box/Box.jsx
+++ components/Objects/Box/Box.jsx
@@ -1,10 +1,13 @@
 
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 export default function Box(props) {
+    const texture = useLoader(TextureLoader, "texture.jpg")
     return (
         <mesh {...props} receiveShadow={true} castShadow={true}>
             <boxBufferGeometry />
-            <meshPhysicalMaterial color={"red"} />
+            <meshPhysicalMaterial map={texture} color={"white"} />
         </mesh>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import { Canvas } from "@react-three/fiber"
 import Floor from '../components/Objects/Floor/Floor'
 import Box from '../components/Objects/Box/Box'
 import Bulb from '../components/Objects/Lightbulb/Bulb'
-import {OrbitControls} from "@react-three/drei"
+import { OrbitControls } from "@react-three/drei"
+import Draggable from '../components/Controls/DragControls'
 // import OrbitControls from '../components/OrbitControls/OrbitControls'
 
 
@@ -17,10 +18,13 @@ export default function Home() {
         carmera={{ position: [-6, 7, 7] }}
       >
         <OrbitControls />
+        <Draggable>
+
+          <Box rotateX={3} rotateY={0.2} position={[0, -4, -5]} />
+        </Draggable>
         <ambientLight color={"white"} intensity={0.3} />
         <Bulb position={[-5, 2, -7]} />
         <Floor position={[0, -5, -5]} />
-        <Box rotateX={3} rotateY={0.2} position={[0, -4, -5]}/>
       </Canvas>
 
     </div>
