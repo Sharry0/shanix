62ad1a104a3ba8b1d531e58dabbdf44700016f15
Sharone Nixon <shanix@live.de>
2022-07-16 14:15:02 +0200

    relocated threeJS examples from index to three page

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,11 @@
 
-// __________ Objects ___________________________________
-import Floor from '../components/Objects/Floor/Floor'
-import Box from '../components/Objects/Box/Box'
-import Bulb from '../components/Objects/Lightbulb/Bulb'
-import Donut from '../components/Objects/Donut/Donut'
-import Caption from '../components/Objects/Text/Caption'
-import MainPlanet from '../components/Objects/Orbit/MainPlanet'
-import Background from '../components/Objects/Orbit/Background'
-import OuterPlanet from "../components/Objects/Orbit/OuterPlanet"
-import Sun from '../components/Objects/Orbit/Sun'
-import Solarsystem from '../components/Objects/Orbit/Solarsystem'
 
-// __________ Controls __________________________________
-import { OrbitControls } from "@react-three/drei"
-import Draggable from '../components/Controls/DragControls'
-
-import css from '../styles/Home.module.css'
-import { Canvas } from "@react-three/fiber"
-import { Suspense } from 'react'
 
 export default function Home() {
   return (
-    <div className={css.scene_container}>
-
-      <div className={css.scene}>
-        <Canvas
-          shadows={true}
-          className={css.canvas}
-          carmera={{ position: [-6, 7, 7] }}
-        >
-          {/* <Draggable> */}
-          <Suspense>
-            {/* <Box position={[0, -1, -1]} /> */}
-            {/* <Donut position={[-4, -1, -6]} rotateX={9} rotateY={4} /> */}
-            <Solarsystem />
-            <Caption>{`this is \n not a test...`} </Caption>
-          </Suspense>
-          {/* </Draggable> */}
-          {/* <Bulb  /> */}
-          <OrbitControls />
-          <ambientLight color={"white"} intensity={0.3} />
-          {/* <Floor position={[0, -5, -5]} /> */}
-        </Canvas>
-        <a href="https://google.com" className={css.link}>to google</a>
-
-      </div>
+    <div >
+      <h1>Home</h1>
+      
     </div>
   )
 }
