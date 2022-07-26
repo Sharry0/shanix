066fa40acf391da1af1c830f78fc8f35bfd424fb
Sharone Nixon <shanix@live.de>
2022-07-26 21:19:18 +0200

    added gsap + delayed monogram animation

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 
 import css from "../styles/Home.module.css"
+import { gsap, Power3 } from "gsap"
 
 export default function Home() {
   return (
     <div className={css.homeContainer} >
 
       <div className={css.svgContainer}>
-        <object className={css.svgObject} type="image/svg+xml" data="monogramAnimated.svg"></object>
+        
+        <object className={css.svgObject} type="image/svg+xml" data="monogramAnimatedLong.svg"></object>
       </div>
 
     </div>
