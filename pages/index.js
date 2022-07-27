6f332ed7e79ec8aa797fd887d65923a42fa81f52
Sharone Nixon <shanix@live.de>
2022-07-27 14:14:56 +0200

    adding animations to svg with gsap

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 
 import css from "../styles/Home.module.css"
-import { gsap, Power3 } from "gsap"
+import LandingSvg from "../components/LandingSvg/LandingSvg"
 
 export default function Home() {
   return (
-    <div className={css.homeContainer} >
+    <div  >
 
-      <div className={css.svgContainer}>
-        
-        <object className={css.svgObject} type="image/svg+xml" data="monogramAnimatedLong.svg"></object>
-      </div>
+      <LandingSvg />
 
     </div>
   )
