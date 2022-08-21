0546f5572e04ceb5c0cd8aa41058d737fae8c953
Sharone Nixon <shanix@live.de>
2022-08-21 15:28:14 +0200

    added comment to footer section

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import css from "./Footer.module.css"
 export default function Footer() {
   return (
     <section className={css.footerSection}>
-        
+        {/* Add links to socials */}
     </section>
   )
 }
