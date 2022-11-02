54b2d5964e22b73e1d9bf6c072b05786bdf2a421
Sharone Nixon <shanix@live.de>
2022-11-02 10:57:04 +0100

    added image slider

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,25 +1,24 @@
 
 import css from "./Projects.module.css"
-import Image from "next/image"
+import ImageSlider from "../ImageSlider/ImageSlider"
 
 export default function Projects() {
+
+  const CgsImages = [
+    "/CGS/CGS_1.png",
+    "/CGS/CGS_2.png",
+    "/CGS/CGS_3.png",
+    "/CGS/CGS_4.png",
+    "/CGS/CGS_5.png",
+    "/CGS/CGS_6.png",
+  ]
+
   return (
     <section className={css.projectsSection} id="projects">
 
 
       <h2>Projects</h2>
-      <div className={css.some}>
-
-        <div className={css.display}>
-          <Image layout="fill" objectFit="contain" src="/CGS/CGS_1.png" alt="" />
-
-        </div>
-        <div className={css.displayBottom} >
-          <div></div>
-
-
-        </div>
-      </div>
+      <ImageSlider images={CgsImages} githubRepo={"https://github.com/Sharry0/CarGoSpace"} />
 
 
     </section>
