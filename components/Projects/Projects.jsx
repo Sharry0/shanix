f88f3bddbdbbd15172897ec9b68fdac93c83640b
Sharone Nixon <shanix@live.de>
2023-01-23 23:17:56 +0100

    added stats project

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -10,18 +10,36 @@ export default function Projects() {
     { src: "/CGS/CGS_3.png" },
     { src: "/CGS/CGS_4.png" },
     { src: "/CGS/CGS_5.png" },
-    { src: "/CGS/CGS_6.png" },
+    { src: "/CGS/CGS_6.png" }
   ];
   const blerryImages = [
     { src: "/Blerry/Blerry_1.png" },
     { src: "/Blerry/Blerry_2.png" },
     { src: "/Blerry/Blerry_3.png" },
-    { src: "/Blerry/Blerry_4.png" },
+    { src: "/Blerry/Blerry_4.png" }
+  ];
+  const statsImages = [
+    { src: "/blacksquareStats/stats_1.png" },
+    { src: "/blacksquareStats/stats_2.png" },
+    { src: "/blacksquareStats/stats_3.png" },
+    { src: "/blacksquareStats/stats_4.png" },
+    { src: "/blacksquareStats/stats_5.png" }
   ];
 
   return (
     <section className={css.projectsSection} id="projects">
       <h2>Projects</h2>
+      <ProjectDisplay
+        imgArr={statsImages}
+        id={"stats"}
+        title={"Statistics"}
+        github={""}
+        website={"https://blacksquare-stats.netlify.app/"}
+        description={<>
+          <p>This is a website that provides charts and data on the NFT project Blacksquare, including real-time market data, historical data, various charts and visualizations to give users an overview. </p>
+          <p>Technologies: Next.js, TypeScript, chakra-ui, ethers, recharts.</p>
+        </>}
+      />
       <ProjectDisplay
         imgArr={cgsImages}
         id={"cgs"}
@@ -44,6 +62,7 @@ export default function Projects() {
           <p>Technologies: React, Web3, ethers, Opensea API, CoinGecko API, MetaMask API</p>
         </>}
       />
+      
     </section>
   )
 }
